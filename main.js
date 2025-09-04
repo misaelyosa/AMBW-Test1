01fa08ca544f04557786df878271a6b3a0bd6cc3
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-09-04 13:22:04 +0700

    create : fetch post from json db

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const currentUser = JSON.parse(localStorage.getItem("authorizedUser"));
 console.log(currentUser);
 
 if (!currentUser) {
+    alert("Anda harus login dulu")
     window.location.href = "auth.html";
 } else {
     document.getElementById("username").textContent = `Hi, ${currentUser.name}`; 
@@ -15,3 +16,46 @@ logout.addEventListener("click", () => {
     alert("Anda berhasil log out");
     window.location.href = "auth.html";
 })
+
+//load post
+const postContainer = document.getElementById("postContainer");
+const postTemplate = document.getElementById("postTemplate");
+
+async function loadPost() {
+    try{
+        const resPosts = await fetch("http://localhost:3000/posts");
+        const posts = await resPosts.json();
+
+        const resComments = await fetch("http://localhost:3000/comments");
+        const comments = await resComments.json();
+
+        postContainer.innerHTML = "";
+        posts.forEach(post => {
+            //setup template
+            const clonePost = postTemplate.content.cloneNode(true);
+
+            //panggil element
+            const displayPostTitle = clonePost.getElementById("displayPostTitle");
+            const displayPostContent = clonePost.getElementById("displayPostContent");
+            const postCommentUl = clonePost.getElementById("postCommentUl");
+            const displayPostComment = clonePost.getElementById("displayPostComment");
+            
+            //set judul , konten
+            displayPostTitle.textContent = post.title;
+            displayPostContent.textContent = post.content;
+
+            //filter komen per post
+            const postComments = comments.filter(c => c.postId === post.id);
+            postComments.forEach(c => {
+                displayPostComment.textContent = c.content;
+                postCommentUl.appendChild(displayPostComment);
+            });
+
+            postContainer.appendChild(clonePost);
+        })
+    } catch (e) {
+        console.error("Post load error : ", e);
+    }
+}
+
+loadPost();
